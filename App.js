9e22cc02e56302fa3d54b6edc580f7f7a8bfe3fc
destroylord <masapin68@gmail.com>
2022-11-01 07:29:22 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import LoginScreen from './src/screens/Login';
+
+function App() {
+  return <LoginScreen />;
+}
+
+export default App;
